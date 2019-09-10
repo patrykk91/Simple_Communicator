728a0b77d9ce687536168a4edb79d64f110a285d
Patryk Kamiński <patrykkaminski@outlook.com>
2019-09-10 21:25:31 +0200

    Login activity implementation

--- app/src/main/java/pl/curlycode/simplecommunicator/LoginActivity.java
+++ app/src/main/java/pl/curlycode/simplecommunicator/LoginActivity.java
@@ -1,4 +1,73 @@
 package pl.curlycode.simplecommunicator;
 
-public class LoginActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    EditText editTextPassword;
+    EditText editTextLogin;
+    Button buttonLogin;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextLogin = findViewById(R.id.editTextLogin);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String login = editTextLogin.getText().toString();
+        final String password = editTextPassword.getText().toString();
+        if (editTextLogin.getText().toString().trim().length() > 0 && editTextPassword.getText().toString().trim().length() > 0) {
+            firebaseAuth.signInWithEmailAndPassword(login, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: ");
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error: " + task.getException(), Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "onComplete: ");
+                        if (task.getException().getMessage().contains("There is no user record corresponding to this identifier. The user may have been deleted.")) {
+                            firebaseAuth.createUserWithEmailAndPassword(login, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(TAG, "onComplete");
+                                    userLoggedIn();
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void userLoggedIn() {
+        Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+        LoginActivity.this.startActivity(intent);
+        LoginActivity.this.finish();
+    }
 }
